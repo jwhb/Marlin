7de95affcd03c01f0202882eaca43ea658bdd1e3
jwhb <jwhy@jwhy.de>
2022-03-17 18:13:41 +0100

    e2v2: update pla preheat preset

--- Configuration Files/E3V2 Templates/BLTouch-3x3/Configuration.h
+++ Configuration Files/E3V2 Templates/BLTouch-3x3/Configuration.h
@@ -1860,8 +1860,8 @@
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "PLA"
-#define PREHEAT_2_TEMP_HOTEND 190
-#define PREHEAT_2_TEMP_BED     55
+#define PREHEAT_2_TEMP_HOTEND 200
+#define PREHEAT_2_TEMP_BED     60
 #define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
 
 #define PREHEAT_3_LABEL       "ABS"
