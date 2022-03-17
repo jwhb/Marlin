c3930575150a6853efd7e17ff105d02d922567f2
jwhb <jwhy@jwhy.de>
2022-03-17 18:11:24 +0100

    e2v2: bed and hotend pid calibration data

--- Configuration Files/E3V2 Templates/BLTouch-3x3/Configuration.h
+++ Configuration Files/E3V2 Templates/BLTouch-3x3/Configuration.h
@@ -602,9 +602,9 @@
     #define DEFAULT_Kd_LIST {  78.81,  78.81 }
   #else
     // Ender 3 v2
-    #define DEFAULT_Kp  28.72
-    #define DEFAULT_Ki   2.62
-    #define DEFAULT_Kd  78.81
+    #define DEFAULT_Kp  30.28
+    #define DEFAULT_Ki   3.44
+    #define DEFAULT_Kd  66.62
   #endif
 #endif // PIDTEMP
 
@@ -642,10 +642,9 @@
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
   // Ender 3 V2
-  #define DEFAULT_bedKp 462.10
-  #define DEFAULT_bedKi  85.47
-  #define DEFAULT_bedKd 624.59
-
+  #define DEFAULT_bedKp 178.67
+  #define DEFAULT_bedKi  34.93
+  #define DEFAULT_bedKd 609.19
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
 
