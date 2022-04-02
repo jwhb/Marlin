9017075daf0dce9ecacc24a94ddb8883f079370c
jwhb <jwhy@jwhy.de>
2022-04-02 21:20:38 +0200

    e3v2: max fan speed 250

--- Configuration Files/E3V2 Templates/BLTouch-3x3/Configuration_adv.h
+++ Configuration Files/E3V2 Templates/BLTouch-3x3/Configuration_adv.h
@@ -536,7 +536,7 @@
  * Define one or both of these to override the default 0-255 range.
  */
 #define FAN_MIN_PWM 130
-#define FAN_MAX_PWM 240
+#define FAN_MAX_PWM 250
 
 /**
  * FAST PWM FAN Settings
