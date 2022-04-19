873d967c7327fac1f6d2e6a678c126dbc933b9be
jwhb <jwhy@jwhy.de>
2022-04-19 10:52:17 +0200

    e3v2: set fan speed to 100%

--- Configuration Files/E3V2 Templates/BLTouch-3x3/Configuration_adv.h
+++ Configuration Files/E3V2 Templates/BLTouch-3x3/Configuration_adv.h
@@ -535,8 +535,8 @@
  *
  * Define one or both of these to override the default 0-255 range.
  */
-#define FAN_MIN_PWM 130
-#define FAN_MAX_PWM 250
+#define FAN_MIN_PWM 255
+#define FAN_MAX_PWM 255
 
 /**
  * FAST PWM FAN Settings
