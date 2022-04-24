3decea5bcea57a07271dfc8694321fdfcadcafd5
jwhb <jwhy@jwhy.de>
2022-04-24 06:35:36 +0200

    e3v2: preheat for leveling

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1902,9 +1902,9 @@
 /**
  * Auto-leveling needs preheating
  */
-//#define PREHEAT_BEFORE_LEVELING
+#define PREHEAT_BEFORE_LEVELING
 #if ENABLED(PREHEAT_BEFORE_LEVELING)
-  #define LEVELING_NOZZLE_TEMP 120   // (°C) Only applies to E0 at this time
+  #define LEVELING_NOZZLE_TEMP  0   // (°C) Only applies to E0 at this time
   #define LEVELING_BED_TEMP     50
 #endif
 
