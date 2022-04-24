7ea4de7055c3ef997d5606acfb186f248b39f39e
jwhb <jwhy@jwhy.de>
2022-04-24 06:40:44 +0200

    e3v2: config for OctoPrint-FirmwareUpdater

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1665,7 +1665,7 @@
   //#define CONFIGURATION_EMBEDDING
 
   // Add an optimized binary file transfer mode, initiated with 'M28 B1'
-  //#define BINARY_FILE_TRANSFER
+  #define BINARY_FILE_TRANSFER
 
   #if ENABLED(BINARY_FILE_TRANSFER)
     // Include extra facilities (e.g., 'M20 F') supporting firmware upload via BINARY_FILE_TRANSFER
@@ -1681,7 +1681,7 @@
    *
    * :[ 'LCD', 'ONBOARD', 'CUSTOM_CABLE' ]
    */
-  //#define SDCARD_CONNECTION LCD
+  #define SDCARD_CONNECTION ONBOARD
 
   // Enable if SD detect is rendered useless (e.g., by using an SD extender)
   //#define NO_SD_DETECT
